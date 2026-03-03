b0ba57184d17cb8a9339b47fdb2897bd10d406a4
Châu Nhật Trường <125680820+truong-magnus@users.noreply.github.com>
2026-03-03 08:49:16 +0700

    feat: create user service and register with eureka

---
+++ user-service/src/main/java/com/elearning/userservice/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.elearning.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // This annotation enables the microservice to register with Eureka Server
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
